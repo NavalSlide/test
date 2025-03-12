2996e5c9541782f764206c5bb8b2799a7f9fab96
NavalSlide <mortisclips999@gmail.com>
2025-03-12 04:58:44 +0000

    xd

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Actualizar paquetes e instalar Chrome
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
+
+# Instalar ChromeDriver
+CHROME_VERSION=$(google-chrome --version | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
+wget -N https://chromedriver.storage.googleapis.com/$(wget -q -O - https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION})/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/local/bin/
+sudo chmod +x /usr/local/bin/chromedriver
+
+# Instalar dependencias de Python con el pip del sistema
+python3 -m pip install --upgrade pip
+python3 -m pip install selenium
